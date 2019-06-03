71905ec158ae368b3dbc1d564fbc9d0364c7df62
Jos van Reenen <jos.vanreenen@hu.nl>
2019-06-03 23:09:15 +0200

    init
    init maven i hope

---
+++ src/main/java/nl/hu/v1wac/got_wac/App.java
@@ -0,0 +1,13 @@
+package nl.hu.v1wac.got_wac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
