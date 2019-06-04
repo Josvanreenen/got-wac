b51dc845049f77e7e1de52b57d11b324e0f0d0d6
jos.vanreenen <jos.vanreenen@C1405866.medewerkers.ad.hvu.nl>
2019-06-04 11:10:46 +0200

    cope with random errors

--- src/main/webapp/js/callingall.js
+++ src/main/webapp/js/callingall.js
@@ -1,8 +1,12 @@
 //fetch loslaten op een METHOD op een URI
 //antwoord toevoegen aan de pagina
 fetch("/rest/GOT").then(response => {
-	console.log(response)
-	response.json()
+	if(response.ok){	return response.json()
+	}
+	else {
+		var head = document.createElement("h1")
+		head.appendChild(document.createTextNode("something went horribly wrong, is it random???"))
+		document.querySelector("body").appendChild(head)}
 	})
 	.then(myJson=>{
 	document.querySelector("body").appendChild(document.createElement("h1").appendChild(document.createTextNode(myJson.answer)))
