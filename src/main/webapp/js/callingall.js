222dcee74c95ec5146bd530ee224e18100793a5e
jos.vanreenen <jos.vanreenen@C1405866.medewerkers.ad.hvu.nl>
2019-06-04 11:06:25 +0200

    logging online

--- src/main/webapp/js/callingall.js
+++ src/main/webapp/js/callingall.js
@@ -1,5 +1,9 @@
 //fetch loslaten op een METHOD op een URI
 //antwoord toevoegen aan de pagina
-fetch("/rest/GOT").then(response => response.json()).then(myJson=>{
+fetch("/rest/GOT").then(response => {
+	console.log(response)
+	response.json()
+	})
+	.then(myJson=>{
 	document.querySelector("body").appendChild(document.createElement("h1").appendChild(document.createTextNode(myJson.answer)))
 })
