2f1b9c03294cbe88982bd0108900bbfc4466c102
jos.vanreenen <jos.vanreenen@C1405866.medewerkers.ad.hvu.nl>
2019-06-04 11:01:44 +0200

    random error message implemented

--- src/main/java/nl/hu/v1wac/got_wac/webservices/GOTResource.java
+++ src/main/java/nl/hu/v1wac/got_wac/webservices/GOTResource.java
@@ -1,11 +1,14 @@
 package nl.hu.v1wac.got_wac.webservices;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Random;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import sun.security.provider.certpath.OCSPResponse.ResponseStatus;
 
 @Path("/GOT")
 public class GOTResource {
@@ -13,6 +16,9 @@ public class GOTResource {
 	@GET
 	@Produces("application/json")
 	public Response baseline() {
+		if(Math.random()>=0.5) {
+			return Response.status(Response.Status.PAYMENT_REQUIRED).build();
+		}
 		SimpleEntry<String, String> resp = new SimpleEntry<String, String>("answer", "look, I did something");
 		return Response.ok(resp).build();
 	}
